a383335e84b35122f6a1a5ab254e94c1b47bff1e
Nihar Mauskar <nihar.mauskar@gmail.com>
2016-10-02 00:02:15 -0700

    Added answer storage

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.use(flash());
 app.use(bodyparser());
 Promise.promisifyAll(require("mongoose"));
 
+var increment = 0;
+
 //databse intialization
 mongoose.connect("mongodb://heroku_wh98625c:hbuo8dj3vbdmpn34l9ij2spm9n@ds047666.mlab.com:47666/heroku_wh98625c");
 var db = mongoose.connection;
@@ -43,24 +45,24 @@ db.once('open', function() {
 		LectureID: [{type: mongoose.Schema.Types.ObjectId}],
 		numLecture: String
 	});
-
-	//defining Lecture Schema
-	var LectureSchema = new mongoose.Schema({
-		LectureName: String,
-		numQuestions: String,
-		Questions: [{
-			QuestionText: String,
-			QuestionID: String,
-			Answer: String
-		}]
-	});
-
+	
 	//defining answer schema
 	var AnswerSchema = new mongoose.Schema({
 		PID: String,
 		QuestionID: String,
 		Answer: String
-	})
+	});
+
+	//defining Lecture Schema
+	var LectureSchema = new mongoose.Schema({
+		LectureName: String,
+		numQuestions: String,
+		QuestionText: [String],
+		QuestionID: [String],
+		Answer: [String],
+		Responses: [AnswerSchema]
+	});
+
 
 	var prof = mongoose.model('Professor', ProfSchema);
 	var course = mongoose.model('Class', ClassSchema);
@@ -244,20 +246,28 @@ db.once('open', function() {
 				
 				var lectures = [];
 				var questions = [];
-				console.log(newCourse);
+				var questionNums = [];
+				var lecIds = [];
 				var allLectures = newCourse.LectureID;
 				for(var id = 0; id < allLectures.length; id++){
 					lec.findById(allLectures[id], function(err, found){
 						if(err) return err;
 						lectures.push(found.LectureName);
 						questions.push(found.Questions);
+						questionNums.push(Number(found.numQuestions));
+						console.log(found.numQuestions);
+						lecIds.push(found._id);
 					});
 				}	
 				setTimeout(function(){
+					console.log(questionNums);
 					res.render('pages/lectures', {
 						ClassName : newCourse.ClassName,
 						lectures : lectures,
-						Questions : questions
+						Questions : questions,
+						classID : req.body.classID,
+						questionNums : questionNums,
+						LecID : lecIds
 					});
 				}, 2000);
 			});
@@ -267,8 +277,89 @@ db.once('open', function() {
 		}
 	});
 
-	//handling creating a new lecture object
-	//app.post('
+	//creating new lecture
+	app.post('/createLecture', function(req, res){
+		if(req.isAuthenticated()){
+
+			course.findOne({
+				'_id': req.body.classID
+			}, function(err, newCourse){
+				if(err) return err;
+				//creates new uniq class
+				var newLec = new lec({
+					LectureName : req.body.lecName,
+					numQuestions : req.body.numQuestions,
+				});
+				
+				var lectures = [];
+				var questions = [];
+				var questionNums = [];
+				var allLectures = newCourse.LectureID;
+				var Lecid = [];
+
+				newLec.save(function (err, course) {
+					if (err) return console.error(err);
+					newCourse.LectureID.push(course._id);
+					newCourse.save();
+					lectures.push(course.LectureName);
+					Lecid.push(course._id);
+					questionNums.push(course.numQuestions);
+				});
+				
+				for(var id = 0; id < allLectures.length; id++){
+					lec.findById(allLectures[id], function(err, found){
+						if(err) return err;
+						console.log(found.LectureName);
+						lectures.push(found.LectureName);
+						questions.push(found.Questions);
+						Lecid.push(found._id);
+						questionNums.push(found.numQuestions);
+					});
+				}	
+				setTimeout(function(){
+					console.log(questionNums);
+					res.render('pages/lectures', {
+						ClassName : newCourse.ClassName,
+						lectures : lectures,
+						Questions : questions,
+						classID : req.body.classID,
+						questionNums : questionNums,
+						LecID : Lecid
+					});
+				}, 2000);
+			});
+		}
+		else {
+			res.render('pages/index');
+		}
+	});
+	
+	//handling creating a new series of questions for lecture
+	app.post('/createQuestions', function(req, res){
+		if(req.isAuthenticated()){
+			lec.findOne({
+				'_id' : req.body.LecID
+			}, function(err, lecture){
+				
+				//updating answer and question values
+				lecture.QuestionText = req.body.Qtext;
+				lecture.Answer = req.body.Qans;
+				var IDlist = [];
+				for(var i = 0; i < Number(lecture.numQuestions); i++){
+					lecture.QuestionID.push((increment++).toString());	
+				}
+				setTimeout(function(){
+					console.log(lecture);
+					lecture.save();
+					res.render('pages/index');
+				}, 3000);
+		
+			});
+		} else {
+			res.render('pages/index');
+		}
+	});
+
 	//handling iphone posts
 	app.post('/iphone', function(req, res){
 		console.log(req.body);
